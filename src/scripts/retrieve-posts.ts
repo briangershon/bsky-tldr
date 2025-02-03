993d17ba79f264d6a6a887244d728242046b656e
Brian Gershon <briangershon@users.noreply.github.com>
2025-02-02 20:47:48 -0800

    Filter posts by day. Short-circuit based based on day.

--- src/scripts/retrieve-posts.ts
+++ src/scripts/retrieve-posts.ts
@@ -15,21 +15,32 @@ async function main() {
   const service = new BskyTldr(bluesky);
 
   const authorDid = 'brianfive.xyz';
+  const targetDate = '20250201';
+  // const authorDid = 'nodejs.org';
+  // const targetDate = '20250121';
 
   const follows = service.retrieveFollowsGenerator({ actor: authorDid });
 
   let followIndex = 0;
   for await (const follow of follows) {
-    console.log('follow', followIndex, follow);
+    const posts = service.retrieveAuthorFeedGenerator({ actor: follow.did });
+
+    console.log(`Retrieving posts for follow ${follow.handle}`);
+
+    let postIndex = 0;
+
+    for await (const post of posts) {
+      const createAt = post.createdAt.slice(0, 10).replace(/-/g, '');
+      if (createAt < targetDate) {
+        console.log('\n---\n');
+        break;
+      }
+
+      console.log('post', postIndex, post.createdAt, post.content.slice(0, 25));
+      postIndex++;
+    }
+
     followIndex++;
-  }
-
-  const posts = service.retrieveAuthorFeedGenerator({ actor: authorDid });
-
-  let postIndex = 0;
-  for await (const post of posts) {
-    console.log('post', postIndex, post);
-    postIndex++;
   }
 }
 
