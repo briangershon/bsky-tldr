0663e051138b21946b20e8a17e577e4cd5bfb6ce
Brian Gershon <briangershon@users.noreply.github.com>
2025-02-17 15:39:55 -0800

    Add `uriToUrl()` helper to convert URI to a viewable web URL (#4)
    - add uriToUrl() helper to convert URI to a viewable web URL
    - update scripts/retrieve-posts example to use uriToUrl()
    - upgrade to @atproto/api v0.14.1
    
    Other:
    
    - reorganize index.ts barrel file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,24 @@
-export { BskyTldr, type Follow, type Post } from './lib/bsky-tldr';
-export {
-  getDailyPostsFromFollows,
-  type DailyPostsFromFollows,
-  type DailyPostsFromFollowsResponse,
+// Import types
+import type { Follow, Post } from './lib/bsky-tldr';
+import type {
+  AuthorFeed,
+  DailyPostsFromFollows,
+  DailyPostsFromFollowsResponse,
 } from './lib/getDailyPostsFromFollows';
+
+// Import functions and classes
+import { BskyTldr } from './lib/bsky-tldr';
+import { getDailyPostsFromFollows } from './lib/getDailyPostsFromFollows';
+import { uriToUrl } from './lib/uriToUrl';
+
+// Types
+export type {
+  Follow,
+  Post,
+  AuthorFeed,
+  DailyPostsFromFollows,
+  DailyPostsFromFollowsResponse,
+};
+
+// Core functionality
+export { BskyTldr, getDailyPostsFromFollows, uriToUrl };
