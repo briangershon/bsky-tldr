832d3f30a96dce21ab914e2a718351c0cf62e996
Brian Gershon <briangershon@users.noreply.github.com>
2025-04-11 22:25:27 -0700

    Fully remove getDailyPostsFromFollows since replaced by utility functions

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,12 @@
 // Import types
 import type { Follow, Post } from './lib/bsky-tldr';
-import type {
-  AuthorFeed,
-  DailyPostsFromFollows,
-  DailyPostsFromFollowsResponse,
-} from './lib/getDailyPostsFromFollows';
 
 // Import functions and classes
 import { retrieveAuthorFeed, retrieveFollows } from './lib/bsky-tldr';
-import { getDailyPostsFromFollows } from './lib/getDailyPostsFromFollows';
 import { uriToUrl } from './lib/uriToUrl';
 
 // Types
-export type {
-  AuthorFeed,
-  DailyPostsFromFollows,
-  DailyPostsFromFollowsResponse,
-  Follow,
-  Post,
-};
+export type { Follow, Post };
 
 // Core functionality
-export {
-  getDailyPostsFromFollows,
-  retrieveAuthorFeed,
-  retrieveFollows,
-  uriToUrl,
-};
+export { retrieveAuthorFeed, retrieveFollows, uriToUrl };
