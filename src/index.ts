6d4870cb3a4eb1e9d7af46843f0b25e97f4d02b5
Brian Gershon <briangershon@users.noreply.github.com>
2025-04-11 22:27:36 -0700

    Condense index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,5 @@
-// Import types
 import type { Follow, Post } from './lib/bsky-tldr';
-
-// Import functions and classes
 import { retrieveAuthorFeed, retrieveFollows } from './lib/bsky-tldr';
 import { uriToUrl } from './lib/uriToUrl';
-
-// Types
-export type { Follow, Post };
-
-// Core functionality
 export { retrieveAuthorFeed, retrieveFollows, uriToUrl };
+export type { Follow, Post };
